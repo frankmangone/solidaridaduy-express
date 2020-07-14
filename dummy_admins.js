08698ca9e3a8986414de0d221bc9d62f64751bed
Franco Mangone <francomangone18@gmail.com>
2020-07-14 11:26:56 -0300

    Added bcrypt and started working on admin login

---
+++ dummy_admins.js
@@ -0,0 +1,15 @@
+var dummy_admins = [{
+	_id: 'aaaaaaaaaaaaaaa',
+	email: 'francomangone18@gmail.com',
+	first_name: 'Franco',
+	last_name: 'Mangone',
+	password: 'tuvieja'
+},{
+	_id: 'bbbbbbbbbbbbbbb',
+	email: 'fbueno@fing.edu.uy',
+	first_name: 'Florencia',
+	last_name: 'Bueno',
+	password: 'solidaridaduy'
+}];
+
+exports.admins = dummy_admins;
--- index.js
+++ index.js
@@ -1,19 +1,54 @@
-var express = require('express');
-var cors    = require('cors')
-var app = express();
+const express = require('express');
+const bcrypt  = require('bcrypt');
+const body_parser = require('body-parser');
+const cors    = require('cors');
+const salt_rounds = 10;
 
+const app     = express();
+
+//app.use(bodyParser.urlencoded({ extended: true }));
+app.use(body_parser.json());
 app.use(cors());
 
-var dummy_initiatives = require('./dummy_initiatives');
-var initiatives = dummy_initiatives.initiatives;
+
+var initiatives = require('./dummy_initiatives');
+initiatives = initiatives.initiatives;
+
+var admins = require('./dummy_admins');
+admins = admins.admins
+for (var i = 0; i < admins.length; i++) {
+	admins[i].password_digest = bcrypt.hashSync(admins[i].password, salt_rounds);
+}
+
 //
 
+// Admin Routes ---------------------------------------------------
 
-app.get('/initiative/:id', function(req, res, next) {
+app.post('/admin/login', function(req, res) {
+	// Request body should be { email, password }
+	let admin = admins.find(adm => adm.email == req.body.email);
+	if (!!admin) {
+		bcrypt.compare(req.body.password, admin.password_digest, function(err, result) {
+			if (result) {
+				res.json(admin)
+			}
+			else {
+				res.json({ validation_errors: { password: 'INVALID' } })
+			}
+		})
+	}
+	else {
+		res.json({ validation_errors: { email: 'INVALID' } })
+	}
+})
+
+// initiative Routes ----------------------------------------------
+
+app.get('/initiative/:id', function(req, res) {
 	res.json(initiatives.find(init => init._id == req.params.id));
 })
 
-app.get('/initiatives/search', function(req, res, next) {
+app.get('/initiatives/search', function(req, res) {
 	res.json(initiatives);
 })
 
